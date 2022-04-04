8a385f900bbe0376a59fe492769a3fb77f548278
UmitUmit <umit.green@gmail.com>
2022-04-04 19:15:34 -0400

    ParallelTest2

--- src/main/java/paralleltesting/ParallelTest1.java
+++ src/main/java/paralleltesting/ParallelTest1.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class ParallelTest1 {
@@ -36,7 +36,7 @@ public class ParallelTest1 {
         Thread.sleep(5000);
     }
 
-  @AfterTest
+  @AfterMethod
     void tearDown()
   {
       driver.quit();
